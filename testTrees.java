6d53036dd06235a394511f82e66519fa5b95920c
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-06-27 16:29:25 -0500

    #1 start point for interactive

--- testTrees.java
+++ testTrees.java
@@ -1,8 +1,8 @@
-import javax.naming.BinaryRefAddr;
-
+import java.util.*;
 public class testTrees{
 
     public static void main(String[]args){
+        ArrayList<String> fastFood = new ArrayList<String> (Arrays.asList("McDonald's","Burger King", "Taco Bell","Wendy's"));
         Node first = new Node("First");
         BinaryTree tree = new BinaryTree(first);
         tree.setLeft(new Node("Second"));
@@ -12,5 +12,18 @@ public class testTrees{
         // tree.moveCurrentRight();
         // System.out.println(tree.getCurrent().data);
         tree.printTree();
+
+        runFavorites(fastFood);
+    }
+
+    public static void runFavorites(ArrayList<String> list){
+        Random rand = new Random();
+        int start = rand.nextInt(list.size());
+        String root = (list.get(start));
+        list.remove(start);
+
+        BinaryTree tree = new BinaryTree(new Node(root));
+        tree.printTree();
+        //System.out.println(list.get(start));
     }
 }
