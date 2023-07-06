7f547c825065d2b57fae8ec2ad59dfa4aa0169d6
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-07-06 01:41:07 -0500

    #4 red bar moves!!

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,16 +2,18 @@
 //     alert('AUUUUUUGHH');
 // })
 let x =1;
+let width = 20;
 const element = document.getElementById("demo");
 setInterval(test, 1000);
 
 function test(){
     element.innerHTML =x
     x++;
-}
-
-var bar = document.getElementById('bar');
+    var bar = document.getElementById('bar');
     if(bar && bar.style){
         bar.style.height = '20px';
-        bar.style.width = '100px';
+        bar.style.width = width+'px';
     }
+    width++;
+}
+
