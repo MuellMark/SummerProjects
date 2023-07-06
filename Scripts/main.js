755384e6f5a5ea85e8c589a29f2197a4343594bd
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-07-06 00:57:38 -0500

    #4 adding setInterval

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,17 +1,16 @@
-clickme.addEventListener('click',function(){
-    alert('AUUUUUUGHH');
-})
+// clickme.addEventListener('click',function(){
+//     alert('AUUUUUUGHH');
+// })
 
+const element = document.getElementById("demo");
 document.getElementById("demo").innerHTML = "AAA";
-
-function test(){
-    let i =true;
+//
+    setInterval(callInterval(),1000);
+function callInterval(){
     let num=1;
     //while(i){
-        //document.getElementById("demo").innerHTML = num;
-        let num2=2;
-        wait(2000);
-        document.getElementById("demo").innerHTML = num2;
+        element.innerHTML+="num";
+        //document.getElementById("demo").innerHTML = num2;
     //}
 
     //document.getElementById("demo").innerHTML = "whaaat";
